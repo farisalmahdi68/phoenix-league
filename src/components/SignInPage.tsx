91977e94c9d7820457f28e0d1f70692732c900d1
farisalmahdi68 <faris.almahdi68@gmail.com>
2025-12-10 22:48:02 +0300

    Update API endpoint for sign-in request

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -34,7 +34,7 @@ export default function SignInPage({ onNavigate, onSignInSuccess }: SignInPagePr
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:3001/api/signin', {
+      const response = await fetch('https://phoenix-league-backend.onrender.com/api/signin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
